68d6515a79e2f8f047ff69a48440d7302838bba1
erinjhu <140906428+erinjhu@users.noreply.github.com>
2025-07-03 12:17:45 -0400

    Set up test on localhost

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! hahahahh')
+})
+
+app.get('/api/health', (req, res) => {
+    res.json(
+        {
+            message: "hi",
+            timestamp: new Date()
+        }
+    )
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

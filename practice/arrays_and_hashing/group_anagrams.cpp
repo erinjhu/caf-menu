37713ff09e388fb175412290023a71154d4350da
erinjhu <140906428+erinjhu@users.noreply.github.com>
2025-07-23 13:59:30 -0400

    close but has logic error

--- practice/arrays_and_hashing/group_anagrams.cpp
+++ practice/arrays_and_hashing/group_anagrams.cpp
@@ -5,90 +5,62 @@
 using namespace std;
 
 
+#include <unordered_map>
+using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-        vector<vector<string>> return_array;
-        int word_count{};
-
-        while(strs.size() != 0){
-
-            vector<string> group_array;
-
-            // put the first word into the anagram group array
-            group_array.push_back(strs.front());
-            // sort the first word and store it in a variable
-            string sort_first_word = strs.front();
-            sort(sort_first_word.begin(), sort_first_word.end());
-            cout << "sort_first_word: " << sort_first_word << endl;
-            // remove the first word from strs
-            strs.erase(strs.begin());
-            cout << "strs: ";
-            cout << "[";
-            for (const auto& word : strs) {
-                cout << word << ", ";
-            }
-            cout << "]" << endl;
-            cout << "group_array: ";
-            cout << "[";
-            for (const auto& word : group_array) {
-                cout << word << ", ";
-            }
-            cout << "]" << endl;
-            word_count++;
-            // compare the first word to the rest of the words in strs
-            for (int k{}; k < strs.size(); k++) {
-                string sort_word = strs[k];
-                sort(sort_word.begin(), sort_word.end());
-                cout << "sort_word: " << sort_word << endl;
-                // if the sorted word matches the sorted first word
-                if(sort_word == sort_first_word){
-                    // add the ptr word to the group array
-                    group_array.push_back(strs[k]);
-                    strs.erase(strs.begin() + k);
-                    cout << "updated group_array: ";
-                    cout << "[";
-                    for (const auto& word : group_array) {
-                        cout << word << ", ";
-                    }
-                    cout << "]" << endl;
-                }
-            }
-            // add group array to return array
-            return_array.push_back(group_array);
-            cout << "updated return array" << endl;
-            for (const auto& group : return_array) {
-                cout << "[";
-                for (const auto& word : group) {
-                    cout << word << ", ";
-                }
-                cout << "],";
+        // hash map <sorted word, array of original groups>
+        unordered_map<string, vector<string>> map;
+        // iterate through strs
+        for(const string &str : strs) {
+            // add the first word to hash map after sorting it
+            string sorted_first_word = strs.front();
+            sort(sorted_first_word.begin(), sorted_first_word.end());
+            cout << "sorted_first_wprd: " << sorted_first_word << endl;
+            vector<string> insert_array;
+            insert_array.push_back(strs.front());
+            map.insert({sorted_first_word, insert_array});
+            cout << "map: " << endl;
+            for( auto it : map) {
+                cout << it.first << " | ";
+                for(auto str : it.second) {
+                    cout << str << ",";
+                } 
             }
             cout << endl;
-
+            // remove the first word from strs
+            strs.erase(strs.begin());
+            // compare the first word in the hash map to the rest 
+            for(int k{}; k < strs.size(); k++){
+                string sorted_next_word = strs[k];
+                sort(sorted_next_word.begin(), sorted_next_word.end());
+                cout << "comparing " << strs[k] << endl;
+            // if the sorted word matches, put in the same hash map slot
+                if(sorted_next_word == sorted_first_word){
+                    map[sorted_first_word].push_back(strs[k]);
+                }
+            }
         }
-
+        vector<vector<string>> return_array;
+        for(auto it = map.begin(); it != map.end(); it++) {
+            return_array.push_back(map[it->first]);
+        }
         return return_array;
     }
 };
 
-int main() {
+int main(){
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
     Solution sol;
-    vector<string> strs = { "cat", "nob", "tac", "bar", "atc", "rab", "bon" };
-
-    cout << "original array" << endl;
-    for (const auto& word : strs) {
-            cout << word << " ";
-    }
-    cout << endl;
-
-    vector<vector<string>> grouped = sol.groupAnagrams(strs);
-    cout << "final array" << endl;
-    for (const auto& group : grouped) {
-        for (const auto& word : group) {
-            cout << word << " ";
+    vector<vector<string>> final = sol.groupAnagrams(strs);
+    cout << "final answer: " << endl;
+    for(auto inner : final){
+        cout << "[ ";
+        for(auto word: inner){
+            cout << word << ",";
         }
+        cout << "], ";
     }
-    cout << endl;
+    return 0;
 }
